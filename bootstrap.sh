504a4d41faa4fa34f86483f07b027bb01c03798d
Mario Sanchez Prada <mario@endlessm.com>
2016-09-27 11:59:06 +0100

    Removed unnecessary provisioning instructions
    Those come from a different Vagrant project I based this one upon.

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,25 +26,3 @@ apt-get install -y --assume-yes \
         libasan1-armhf-cross \
         libgcc-4.9-dev-armhf-cross \
         libstdc++-4.9-dev-armhf-cross
-
-# Flatpak runtimes
-# ----------------
-
-tmpdir=$(mktemp -d) && pushd ${tmpdir}
-
-wget -O gnome-sdk.gpg https://sdk.gnome.org/keys/gnome-sdk.gpg && \
-{
-    flatpak remote-add --gpg-import=gnome-sdk.gpg gnome https://sdk.gnome.org/repo
-    flatpak install gnome org.gnome.Platform 3.22
-    flatpak install gnome org.gnome.Sdk 3.22
-}
-
-popd && rm -rf ${tmpdir}
-
-# Additional configuration
-# ------------------------
-
-# Some modules installed with npm install look for node
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-
-
